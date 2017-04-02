e63d4cf72bb41707dc3ce67261c5047211713c96
saitoiotken <saitoiotken@users.noreply.github.com>
2017-04-02 13:07:22 +0900

    fix graph title

--- mpu9250py/mpu9250py.py
+++ mpu9250py/mpu9250py.py
@@ -53,17 +53,17 @@ class GraphManager:
 
 def pause_plot():
     #init garaph
-    ge_acc = GraphManager(3)
-    ge_gyr = GraphManager(3)
-    ge_mag = GraphManager(3)
-    ge_temp = GraphManager(1)
+    gm_acc = GraphManager(3)
+    gm_gyr = GraphManager(3)
+    gm_mag = GraphManager(3)
+    gm_temp = GraphManager(1)
 
-    fig, ([ge_acc.axis, ge_gyr.axis], [ge_mag.axis, ge_temp.axis])= plt.subplots(nrows=2,ncols=2, figsize=(12,8))
+    fig, ([gm_acc.axis, gm_gyr.axis], [gm_mag.axis, gm_temp.axis])= plt.subplots(nrows=2,ncols=2, figsize=(12,8))
     
-    ge_acc.setup("加速度 (g)", ["x", "y", "z"], -2, 2, 0.001)
-    ge_gyr.setup("ジャイロ (°/sec)", ["x", "y", "z"], -250, 250, 0.001*250)
-    ge_mag.setup("磁気 (uT)", ["x", "y", "z"], -100, 100, 1.0)
-    ge_temp.setup("温度 (℃)", ["t"], 0, 50, 0.1)
+    gm_acc.setup("Acceleration (g)", ["x", "y", "z"], -2, 2, 0.001)
+    gm_gyr.setup("Gyro (°/sec)", ["x", "y", "z"], -250, 250, 0.001*250)
+    gm_mag.setup("Magnetic (uT)", ["x", "y", "z"], -100, 100, 1.0)
+    gm_temp.setup("Temperature (°C)", ["t"], 0, 50, 0.1)
 
     while True:
         #read serial
@@ -71,22 +71,22 @@ def pause_plot():
         
         if re.search(r"ACC:", line):
             accs = re.findall(r"\d{1,}", line)
-            ge_acc.update(accs)
+            gm_acc.update(accs)
 #            print ("ACC:%s" % accs)
             continue
         if re.search(r"GYR:", line):
             grys = re.findall(r"\d{1,}", line)
-            ge_gyr.update(grys)
+            gm_gyr.update(grys)
 #            print ("GYR:%s" % grys)
             continue
         if re.search(r"MAG:", line):
             mags = re.findall(r"\d{1,}", line)
-            ge_mag.update(mags)
+            gm_mag.update(mags)
 #            print ("MAG:%s" % mags)
             continue
         if re.search(r"T:", line):
             temps = re.findall(r"\d{1,}", line)
-            ge_temp.update(temps)
+            gm_temp.update(temps)
 #            print ("T:%s" % temps)
 
             #replot
